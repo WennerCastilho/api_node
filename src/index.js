adbb285bd83e975d88e7bf1b8fb4d5c00f132911
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-05-29 19:20:50 -0300

    Feat: Use controllers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const http = require('http')
-const singers = require('./mocks/singers')
+const SingerController = require('./controllers/SingerController')
+const NotFoundController = require('./controllers/NotFoundController')
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
   if (request.url === '/singers' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(singers))
+    SingerController.listSingers(request, response)
+  } else {
+    NotFoundController(request, response)
   }
 
-  response.writeHead(200, { 'Content-Type': 'application/json'})
-  response.end(JSON.stringify({
-    available_routes: ['/singers'],
-  }))
 })
 
 server.listen(3000, () => {
