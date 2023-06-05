49b03079bda2e1c1f8dbd30b5511ef83fc9bfc1e
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-06-05 19:30:03 -0300

    Fix: Improvement NotFound routes

--- src/controllers/NotFoundController.js
+++ src/controllers/NotFoundController.js
@@ -1,6 +1,8 @@
+const routes = require('../routes')
+
 module.exports = (request, response) => {
   response.writeHead(404, { 'Content-Type': 'application/json'})
   return response.end(JSON.stringify({
-    available_routes: ['/singers'],
+    available_routes: routes.map(({ endpoint, method }) => ({ endpoint, method })),
   }))
 }
