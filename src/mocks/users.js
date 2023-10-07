05af2251251736174f5ab53530657a559d9a386a
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-10-07 19:40:46 -0300

    Feat: Created route to Users

---
+++ src/mocks/users.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 1, nome: 'João' },
+  { id: 2, nome: 'Maria' },
+  { id: 3, nome: 'Pedro' },
+  { id: 4, nome: 'Ana' },
+  { id: 5, nome: 'Luiza' },
+  { id: 6, nome: 'Rafael' }
+];
+
+module.exports = users
