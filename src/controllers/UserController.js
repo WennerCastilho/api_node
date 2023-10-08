3b734a4ab471eaf02998d9c8e27242a498ec9aaa
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-10-07 23:04:13 -0300

    Feat: POST, PUT and helpers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
-const users = require('../mocks/users')
+const bodyParser = require('../helpers/bodyParser')
+let users = require('../mocks/users')
 
 module.exports = {
   listUsers(request, response) {
@@ -18,6 +19,41 @@ module.exports = {
     if (user) {
       return response.send(200, { user })
     }
-      response.send(400, { error: "User not found" })
-    },
+    response.send(400, { error: "User not found" })
+  },
+  createUser(request, response) {
+    const { body } = request
+    const lastUserId = users[users.length - 1].id
+
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    }
+
+    users.push(newUser)
+
+    response.send(200, newUser)
+  },
+  updateUser(request, response) {
+    let { id } = request.param
+    const { name } = request.body
+    const { body } = request
+
+    id = Number(id)
+
+    let userExists = users.find((userObj) => userObj.id === id)
+
+    if (!userExists) return response.send(200, { error: 'User not found' })
+
+    userExists.name = body.name
+      users = users.map((user) => {
+        if (user.id === id) ({
+          id,
+          name: body,
+        })
+        return user
+      })
+
+      response.send(200, userExists)
   }
+}
--- src/mocks/users.js
+++ src/mocks/users.js
@@ -1,10 +1,10 @@
 const users = [
-  { id: 1, nome: 'João' },
-  { id: 2, nome: 'Maria' },
-  { id: 3, nome: 'Pedro' },
-  { id: 4, nome: 'Ana' },
-  { id: 5, nome: 'Luiza' },
-  { id: 6, nome: 'Rafael' }
+  { id: 1, name: 'João' },
+  { id: 2, name: 'Maria' },
+  { id: 3, name: 'Pedro' },
+  { id: 4, name: 'Ana' },
+  { id: 5, name: 'Luiza' },
+  { id: 6, name: 'Rafael' }
 ];
 
 module.exports = users
