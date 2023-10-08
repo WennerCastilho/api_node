2322875531d9ab41805e7cc391eeea3f9c33fe83
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-10-07 21:05:02 -0300

    Feat: Query params

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const http = require("http")
 const routes = require("./routes");
+const { URL } = require('url')
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+
   const route = routes.find(({ endpoint, method }) => {
-    return request.url === endpoint && request.method === method
+    return parsedUrl.pathname === endpoint && request.method === method
   })
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
